75375c9bdb90c292f046725681566d9efd1443cf
marblerun <root@p-lnvtsfbiheaj-workstation.c.instruqt-prod.internal>
2021-10-14 11:32:23 +0000

    Added the second tag

--- main.tf
+++ main.tf
@@ -126,9 +126,10 @@ resource "aws_instance" "hashicat" {
   subnet_id                   = aws_subnet.hashicat.id
   vpc_security_group_ids      = [aws_security_group.hashicat.id]
   Department                  = devops
-
+  
   tags = {
     Name = "${var.prefix}-hashicat-instance"
+    Billable
   }
 }
 
