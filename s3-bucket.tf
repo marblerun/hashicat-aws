605815afb6f882e92c81eee4507486eded076760
marblerun <root@p-lnvtsfbiheaj-workstation.c.instruqt-prod.internal>
2021-10-14 15:20:05 +0000

    Added s3 bucket module again - really

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,6 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   bucket_prefix = "mike-shield"
   version = "2.2.0"
-  bucket = "my-s3-bucket"
   acl    = "private"
 
   versioning = {
