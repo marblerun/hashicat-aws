c1acd747f71e302b3ce249e55f3e989e7118bc2c
marblerun <root@p-lnvtsfbiheaj-workstation.c.instruqt-prod.internal>
2021-10-14 14:52:42 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,16 +1,11 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
-
+  bucket_prefix = "mike-shield"
   bucket = "my-s3-bucket"
   acl    = "private"
 
   versioning = {
     enabled = true
   }
-
-}
-"
-  bucket = "my-s3-bucket"
-  bucket_prefix = "mike-shield"
 
 }
