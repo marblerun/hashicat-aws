f263bb595e2c9143b0d82389f265c0eed1fd744c
marblerun <root@p-lnvtsfbiheaj-workstation.c.instruqt-prod.internal>
2021-10-14 14:46:01 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,5 @@
 module "s3_bucket" {
-  source = "https://github.com/marblerun/terraform-aws-s3-bucket"
-
+  source = "https://github.com/marblerun/terraform-aws-s3-bucket/aws/"
   bucket = "my-s3-bucket"
   bucket_prefix = "mike-shield"
 
